cff593216223f5808006c1675464a6d972b8945a
GitHub Action <action@github.com>
2021-07-18 20:26:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/buiminhtamweb/blocgenerate/MyBundle.kt
+++ src/main/kotlin/com/github/buiminhtamweb/blocgenerate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.buiminhtamweb.blocgenerate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/buiminhtamweb/blocgenerate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.buiminhtamweb.blocgenerate.services
+
+import com.github.buiminhtamweb.blocgenerate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
